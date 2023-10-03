c4f3c28f3ad282ed57cf4f7b8eb9695732cda482
Moganesan <developer.moganesan@gmail.com>
2023-10-04 00:47:45 +0530

    biagram model added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import torch.nn as nn
+from torch.nn import functional as F
 # read the text file and store it in text variable
 with open("input.txt","r",encoding="utf-8") as f:
     text = f.read();
@@ -62,4 +64,48 @@ for b in range(batch_size): # batch dimension
         context = xb[b, :t+1]
         target = yb[b,t]
         print(f"when input is {context.tolist()} the target: {target}")
-print(xb) # input to the transformer
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx)  # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :]  # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
+        return idx
+
+
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+print(logits.shape)
+print(loss)
+
+print(decode(m.generate(idx=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
